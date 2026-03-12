484f6fdc7b825293b1608b71086344d43f72e08e
gehud <grozdovnikolay@gmail.com>
2026-03-12 16:52:31 +0300

    feat: dock layout

---
+++ src/dock/dock_state/tree/tab_index.rs
@@ -0,0 +1,11 @@
+﻿/// Identifies a tab within a [`Node`](crate::Node).
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, Ord, PartialOrd)]
+#[derive(serde::Deserialize, serde::Serialize)]
+pub struct TabIndex(pub usize);
+
+impl From<usize> for TabIndex {
+    #[inline]
+    fn from(index: usize) -> Self {
+        TabIndex(index)
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,34 @@
+mod dock;
+mod pane;
+mod style;
+
 use bevy::{
     DefaultPlugins,
-    app::{App, Plugin, PluginGroup},
+    app::{App, Plugin, PluginGroup, PostStartup, Startup},
+    camera::Camera2d,
+    ecs::{
+        error::Result,
+        query::With,
+        system::{Commands, ResMut, Single, SystemState},
+        world::World,
+    },
     utils::default,
     window::{Window, WindowPlugin},
+};
+use bevy_egui::{
+    EguiContext, EguiContexts, EguiGlobalSettings, EguiPlugin, EguiPrimaryContextPass,
+    PrimaryEguiContext,
+};
+use egui::{
+    CentralPanel, FontData, FontFamily, Frame, MenuBar, TopBottomPanel,
+    epaint::text::{FontInsert, FontPriority, InsertFontFamily},
+    panel::TopBottomSide,
+};
+
+use crate::{
+    dock::{DockArea, Style},
+    pane::{Docking, PaneViewer, Panes},
+    style::set_dark_style,
 };
 
 #[derive(Default)]
@@ -16,6 +42,84 @@ impl Plugin for EditorPlugin {
                 ..default()
             }),
             ..default()
-        }));
+        }))
+        .add_plugins(EguiPlugin::default())
+        .init_resource::<Panes>()
+        .init_resource::<Docking>()
+        .add_systems(Startup, setup)
+        .add_systems(EguiPrimaryContextPass, ui);
     }
+}
+
+fn setup(mut commands: Commands, mut egui_global_settings: ResMut<EguiGlobalSettings>) {
+    egui_global_settings.auto_create_primary_context = false;
+    commands
+        .spawn((Camera2d::default(), PrimaryEguiContext))
+        .entry::<EguiContext>()
+        .and_modify(|mut ctx| {
+            ctx.get_mut().style_mut(|style| {
+                set_dark_style(style);
+            });
+
+            ctx.get_mut().all_styles_mut(|style| set_dark_style(style));
+            ctx.get_mut().add_font(FontInsert::new(
+                "fira",
+                FontData::from_static(include_bytes!(
+                    "./assets/fonts/fira_sans/FiraSans-Regular.ttf"
+                )),
+                vec![InsertFontFamily {
+                    family: FontFamily::Proportional,
+                    priority: FontPriority::Highest,
+                }],
+            ));
+        });
+}
+
+fn ui(
+    world: &mut World,
+    ctx: &mut SystemState<Single<&mut EguiContext, With<PrimaryEguiContext>>>,
+) -> Result {
+    let ctx = ctx.get_mut(world).get_mut().clone();
+
+    TopBottomPanel::new(TopBottomSide::Top, "header")
+        .show_separator_line(false)
+        .exact_height(34.0)
+        .show(&ctx, |ui| {
+            ui.horizontal_centered(|ui| {
+                MenuBar::new().ui(ui, |ui| {
+                    ui.menu_button("File", |ui| if ui.button("Open").clicked() {});
+
+                    ui.menu_button("View", |_ui| {});
+                });
+            });
+        });
+
+    TopBottomPanel::new(TopBottomSide::Bottom, "footer")
+        .show_separator_line(false)
+        .exact_height(24.0)
+        .show(&ctx, |_ui| {
+            // TODO: Make footer.
+        });
+
+    CentralPanel::default()
+        .frame(
+            Frame::central_panel(&ctx.style())
+                .inner_margin(0)
+                .outer_margin(0),
+        )
+        .show(&ctx, |ui| {
+            let style = Style::from_egui(ui.style());
+
+            world.resource_scope::<Docking, _>(|world, mut docking| {
+                let mut pane_viewer = PaneViewer { world };
+
+                DockArea::new(&mut docking.0)
+                    .style(style)
+                    .show_leaf_close_all_buttons(false)
+                    .show_leaf_collapse_buttons(false)
+                    .show_inside(ui, &mut pane_viewer);
+            });
+        });
+
+    Ok(())
 }
