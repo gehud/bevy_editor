12e2eff1560fd4c05dc4bb55444f3546cfc2b56a
gehud <grozdovnikolay@gmail.com>
2026-03-24 21:54:02 +0300

    feat: refreshing asset database

--- src/asset/database.rs
+++ src/asset/database.rs
@@ -1,7 +1,11 @@
-use std::{fs::create_dir_all, path::Path, sync::Mutex};
+use std::{
+    fs::{create_dir_all, read_dir},
+    path::Path,
+    sync::{Mutex, MutexGuard, PoisonError},
+};
 
 use bevy::ecs::{error::Result, resource::Resource};
-use rusqlite::Connection;
+use rusqlite::{Connection, params};
 
 #[derive(Resource)]
 pub(super) struct AssetDatabase(Mutex<Connection>);
@@ -13,6 +17,28 @@ impl AssetDatabase {
                 .parent()
                 .ok_or_else(|| "Invalid asset database path.")?,
         )?;
-        Ok(Self(Mutex::new(Connection::open(path.as_ref())?)))
+
+        let db = Self(Mutex::new(Connection::open(path.as_ref())?));
+        db.init()?;
+
+        Ok(db)
+    }
+
+    pub fn connection(&self) -> MutexGuard<'_, Connection> {
+        self.0.lock().unwrap_or_else(PoisonError::into_inner)
+    }
+
+    pub fn init(&self) -> Result {
+        self.connection().execute(
+            "create table if not exists assets (
+                    uuid text not null primary key,
+                    path text not null,
+                    label text default null,
+                    deleted bool not null default false,
+                    modified_at datetime nut null
+                )",
+            (),
+        )?;
+        Ok(())
     }
 }
