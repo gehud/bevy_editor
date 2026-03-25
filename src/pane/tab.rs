2d1251841bf7a70ec326f25635dfd996955769e2
gehud <grozdovnikolay@gmail.com>
2026-03-25 15:36:31 +0300

    refactor: splitting pane plugin

---
+++ src/pane/tab.rs
@@ -0,0 +1,469 @@
+use bevy::{
+    app::{App, Plugin, PostUpdate, Update},
+    asset::AssetServer,
+    camera::{NormalizedRenderTarget, visibility::Visibility},
+    ecs::{
+        change_detection::DetectChanges,
+        component::Component,
+        entity::{ContainsEntity, Entity},
+        error::Result,
+        hierarchy::{ChildOf, Children},
+        observer::On,
+        query::{Changed, Or},
+        system::{Commands, EntityCommands, Query, Res, ResMut},
+        world::Ref,
+    },
+    input_focus::{InputFocus, IsFocused, IsFocusedHelper},
+    log::warn,
+    picking::{
+        Pickable,
+        events::{
+            Cancel, Drag, DragDrop, DragEnd, DragEnter, DragLeave, DragOver, DragStart, Pointer,
+            Press,
+        },
+        pointer::{PointerButton, PointerLocation, PointerMap},
+    },
+    text::TextFont,
+    ui::{
+        AlignItems, ComputedNode, Node, PositionType, ScrollPosition, UiRect, UiScale, percent, px,
+        widget::Text,
+    },
+    utils::default,
+    window::SystemCursorIcon,
+};
+
+use crate::{
+    pane::{Pane, PaneRef, PaneRegistry},
+    theme::{
+        RoundedCorners, ThemeBackgroundColor, ThemeBorderColor, ThemeTextColor,
+        constants::fonts::REGULAR,
+        tokens::{PANE_BG, PANE_TAB_ACTIVE, TEXT_MAIN, WINDOW_BG},
+    },
+    widget::{ContextMenu, ContextMenuMark, EntityCursor, OverrideCursor},
+    window::{EditorWindowAutoFocus, EditorWindowStructure},
+};
+
+#[derive(Component)]
+pub(super) struct PaneTab {
+    pub name: String,
+}
+
+#[derive(Component)]
+pub(super) struct PaneTabbar {
+    pub drop_indicator: Entity,
+    pub tabgroup: Entity,
+}
+
+#[derive(Component)]
+pub(super) struct PaneTabgroup {
+    pub active_tab_index: usize,
+}
+
+#[derive(Component)]
+pub(super) struct DraggedTab {
+    pub indicator: Entity,
+    pub drop_index: usize,
+}
+
+pub(super) fn on_tabbar_drag_enter(
+    trigger: On<Pointer<DragEnter>>,
+    tabbars: Query<&PaneTabbar>,
+    dragged_tabs: Query<&DraggedTab>,
+    mut commands: Commands,
+) -> Result {
+    let tabbar = tabbars.get(trigger.entity)?;
+
+    let Ok(_) = dragged_tabs.get(trigger.dragged) else {
+        return Ok(());
+    };
+
+    commands
+        .entity(tabbar.drop_indicator)
+        .entry::<Visibility>()
+        .and_modify(|mut visibility| {
+            *visibility = Visibility::Inherited;
+        });
+
+    Ok(())
+}
+
+pub(super) fn on_tabbar_drag_over(
+    trigger: On<Pointer<DragOver>>,
+    tabbars: Query<&PaneTabbar>,
+    children: Query<&Children>,
+    scroll_positions: Query<&ScrollPosition>,
+    computed_nodes: Query<&ComputedNode>,
+    mut dragged_tabs: Query<&mut DraggedTab>,
+    mut nodes: Query<&mut Node>,
+) -> Result {
+    let tabbar = tabbars.get(trigger.entity)?;
+
+    let Ok(mut dragged_tab) = dragged_tabs.get_mut(trigger.dragged) else {
+        return Ok(());
+    };
+
+    let tabbar_size = computed_nodes.get(trigger.entity)?.size().x;
+
+    let mut indicator = nodes.get_mut(tabbar.drop_indicator)?;
+
+    let pointer_position = trigger
+        .hit
+        .position
+        .map(|position| (position.x + 0.5) * tabbar_size)
+        .unwrap_or_default();
+
+    let mut indicator_position = 0.0;
+    dragged_tab.drop_index = 0;
+    let tabs = children.get(tabbar.tabgroup)?;
+    let mut dragged_tab_index = None;
+    for (i, tab) in tabs.iter().enumerate() {
+        let size = computed_nodes.get(*tab)?.size().x;
+        if *tab == trigger.dragged {
+            dragged_tab_index = Some(i);
+        }
+
+        if pointer_position < indicator_position + (size / 2.0) {
+            break;
+        }
+
+        indicator_position += size;
+        dragged_tab.drop_index += 1;
+    }
+
+    if let Some(dragged_tab_index) = dragged_tab_index {
+        if dragged_tab.drop_index > dragged_tab_index {
+            dragged_tab.drop_index = dragged_tab.drop_index.saturating_sub(1);
+        }
+    }
+
+    indicator_position -= scroll_positions
+        .get(trigger.entity)
+        .map(|position| position.x)
+        .unwrap_or_default();
+
+    indicator.left = percent(indicator_position / tabbar_size * 100.0);
+
+    Ok(())
+}
+
+pub(super) fn on_tabbar_drag_drop(
+    trigger: On<Pointer<DragDrop>>,
+    tabbars: Query<&PaneTabbar>,
+    dragged_tabs: Query<&DraggedTab>,
+    mut commands: Commands,
+) -> Result {
+    let tabbar = tabbars.get(trigger.entity)?;
+
+    let Ok(dragged_tab) = dragged_tabs.get(trigger.dropped) else {
+        return Ok(());
+    };
+
+    let drop_index = dragged_tab.drop_index;
+
+    commands
+        .entity(tabbar.tabgroup)
+        .insert_child(drop_index, trigger.dropped)
+        .entry::<PaneTabgroup>()
+        .and_modify(move |mut tabgroup| {
+            if tabgroup.active_tab_index != drop_index {
+                tabgroup.active_tab_index = drop_index;
+            }
+        });
+
+    Ok(())
+}
+
+pub(super) fn on_tabbar_drag_leave(
+    trigger: On<Pointer<DragLeave>>,
+    tabbars: Query<&PaneTabbar>,
+    mut commands: Commands,
+) -> Result {
+    let tabbar = tabbars.get(trigger.entity)?;
+
+    commands
+        .entity(tabbar.drop_indicator)
+        .entry::<Visibility>()
+        .and_modify(|mut visibility| {
+            *visibility = Visibility::Hidden;
+        });
+
+    Ok(())
+}
+
+pub(super) fn on_tab_press(
+    trigger: On<Pointer<Press>>,
+    parents: Query<&ChildOf>,
+    mut tabgroups: Query<&mut PaneTabgroup>,
+    children: Query<&Children>,
+    mut focus: ResMut<InputFocus>,
+) -> Result {
+    if trigger.button != PointerButton::Primary {
+        return Ok(());
+    }
+
+    let parent = parents.get(trigger.entity)?.parent();
+    let tabs = children.get(parent)?;
+    let index = tabs
+        .iter()
+        .position(|sibling| *sibling == trigger.entity)
+        .unwrap();
+    let mut tabgroup = tabgroups.get_mut(parent)?;
+    if tabgroup.active_tab_index != index {
+        tabgroup.active_tab_index = index;
+    }
+    focus.set(trigger.entity);
+
+    Ok(())
+}
+
+pub(super) fn on_tab_drag_start(
+    trigger: On<Pointer<DragStart>>,
+    editor_windows: Query<&EditorWindowStructure>,
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    tabs: Query<(&PaneTab, &PaneRef)>,
+    mut auto_focus: ResMut<EditorWindowAutoFocus>,
+    mut override_cursor: ResMut<OverrideCursor>,
+) -> Result {
+    if trigger.button != PointerButton::Primary {
+        return Ok(());
+    }
+
+    let NormalizedRenderTarget::Window(window) = trigger.pointer_location.target else {
+        return Ok(());
+    };
+
+    let Ok(editor_window) = editor_windows.get(window.entity()) else {
+        return Ok(());
+    };
+
+    let (tab, pane) = tabs.get(trigger.entity)?;
+    let indicator = spawn_tab(&mut commands, &asset_server, pane.entity, tab.name.clone())
+        .insert(ChildOf(editor_window.root()))
+        .insert(Pickable::IGNORE)
+        .insert((
+            ThemeBackgroundColor(PANE_BG),
+            ThemeBorderColor::all(PANE_TAB_ACTIVE),
+        ))
+        .entry::<Node>()
+        .and_modify(|mut node| {
+            node.position_type = PositionType::Absolute;
+            node.border = UiRect::all(px(2));
+            node.border_radius = RoundedCorners::All.to_border_radius(2.0);
+        })
+        .entity()
+        .id();
+
+    commands.entity(trigger.entity).insert(DraggedTab {
+        indicator,
+        drop_index: 0,
+    });
+
+    auto_focus.0 = true;
+    override_cursor.0 = Some(EntityCursor::System(SystemCursorIcon::Grabbing));
+
+    Ok(())
+}
+
+pub(super) fn on_tab_drag(
+    trigger: On<Pointer<Drag>>,
+    dragged_tabs: Query<&DraggedTab>,
+    pointer_map: Res<PointerMap>,
+    pointers: Query<&PointerLocation>,
+    editor_windows: Query<&EditorWindowStructure>,
+    parents: Query<&ChildOf>,
+    mut nodes: Query<&mut Node>,
+    ui_scale: Res<UiScale>,
+    mut commands: Commands,
+) -> Result {
+    let Ok(dragged_tab) = dragged_tabs.get(trigger.entity) else {
+        return Ok(());
+    };
+
+    // The drag event uses the initial drag location.
+    // To correctly update the position, we need the current pointer location.
+    let Some(location) = pointers
+        .get(pointer_map.get_entity(trigger.pointer_id).unwrap())?
+        .location
+        .clone()
+    else {
+        return Ok(());
+    };
+
+    let NormalizedRenderTarget::Window(window_ref) = location.target else {
+        return Ok(());
+    };
+
+    let editor_window = editor_windows.get(window_ref.entity())?;
+
+    let indicator_root = parents.get(dragged_tab.indicator)?.parent();
+
+    if indicator_root != editor_window.root() {
+        commands
+            .entity(editor_window.root())
+            .add_child(dragged_tab.indicator);
+    }
+
+    let mut indicator = nodes.get_mut(dragged_tab.indicator)?;
+
+    indicator.left = px(location.position.x / ui_scale.0);
+    indicator.top = px(location.position.y / ui_scale.0);
+
+    Ok(())
+}
+
+pub(super) fn on_tab_drag_end(
+    trigger: On<Pointer<DragEnd>>,
+    dragged_tabs: Query<&DraggedTab>,
+    mut auto_focus: ResMut<EditorWindowAutoFocus>,
+    mut commands: Commands,
+    mut override_cursor: ResMut<OverrideCursor>,
+) {
+    let Ok(dragged_tab) = dragged_tabs.get(trigger.entity) else {
+        return;
+    };
+
+    commands.entity(dragged_tab.indicator).despawn();
+    commands.entity(trigger.entity).remove::<DraggedTab>();
+    override_cursor.0 = None;
+    auto_focus.0 = false;
+}
+
+pub(super) fn on_tab_drag_cancel(
+    trigger: On<Pointer<Cancel>>,
+    dragged_tabs: Query<&DraggedTab>,
+    mut auto_focus: ResMut<EditorWindowAutoFocus>,
+    mut commands: Commands,
+    mut override_cursor: ResMut<OverrideCursor>,
+) {
+    let Ok(dragged_tab) = dragged_tabs.get(trigger.entity) else {
+        return;
+    };
+
+    commands.entity(dragged_tab.indicator).despawn();
+    commands.entity(trigger.entity).remove::<DraggedTab>();
+    override_cursor.0 = None;
+    auto_focus.0 = false;
+}
+
+pub(super) fn spawn_tab<'a>(
+    commands: &'a mut Commands,
+    asset_server: &AssetServer,
+    pane: Entity,
+    tab: String,
+) -> EntityCommands<'a> {
+    let root = commands
+        .spawn((
+            PaneRef { entity: pane },
+            PaneTab { name: tab.clone() },
+            Node {
+                flex_shrink: 0.0,
+                height: px(29),
+                padding: UiRect::horizontal(px(8)),
+                border: UiRect::top(px(2)),
+                border_radius: RoundedCorners::Top.to_border_radius(2.0),
+                align_items: AlignItems::Center,
+                ..default()
+            },
+            Pickable {
+                should_block_lower: false,
+                ..default()
+            },
+            EntityCursor::System(SystemCursorIcon::Pointer),
+            ThemeBackgroundColor(WINDOW_BG),
+            ThemeBorderColor::all(WINDOW_BG),
+        ))
+        .id();
+
+    commands.spawn((
+        Pickable::IGNORE,
+        ChildOf(root),
+        Text::new(tab),
+        TextFont {
+            font: asset_server.load(REGULAR),
+            font_size: 12.0,
+            ..default()
+        },
+        ThemeTextColor(TEXT_MAIN),
+    ));
+
+    commands.entity(root)
+}
+
+pub(super) fn tab_context_menu(tab: Entity) -> ContextMenu {
+    ContextMenu::new().with_option(true, ContextMenuMark::None, "Close", move |world| {
+        world.commands().entity(tab).despawn();
+        Ok(())
+    })
+}
+
+fn focus_tabs(
+    focus: IsFocusedHelper,
+    input_focus: Res<InputFocus>,
+    tabgroups: Query<(&PaneRef, Ref<PaneTabgroup>, Ref<Children>)>,
+    pane_registry: Res<PaneRegistry>,
+    pane_tabs: Query<&PaneTab>,
+    pane_structures: Query<&Pane>,
+    mut commands: Commands,
+) -> Result {
+    for (pane, tabgroup, tabs) in tabgroups {
+        let pane_structure = pane_structures.get(pane.entity)?;
+        let is_tabgroup_changed = tabgroup.is_changed() || tabs.is_changed();
+
+        for (i, tab) in tabs.iter().enumerate() {
+            let is_active = i == tabgroup.active_tab_index;
+
+            if is_tabgroup_changed || input_focus.is_changed() {
+                let is_focused =
+                    focus.is_focused(*tab) || focus.is_focus_within(pane_structure.content);
+
+                commands
+                    .entity(*tab)
+                    .insert(ThemeBackgroundColor(if is_active {
+                        PANE_BG
+                    } else {
+                        WINDOW_BG
+                    }))
+                    .insert(ThemeBorderColor::all(if is_active {
+                        if is_focused { PANE_TAB_ACTIVE } else { PANE_BG }
+                    } else {
+                        WINDOW_BG
+                    }));
+            }
+
+            if is_active && is_tabgroup_changed {
+                let tab_name = &pane_tabs.get(*tab)?.name;
+
+                commands.entity(pane_structure.content).despawn_children();
+
+                if let Some(system) = pane_registry.get(tab_name) {
+                    commands.run_system_with(system, *pane_structure);
+                } else {
+                    warn!("Missing tab pane: {}", tab_name);
+                }
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn clamp_active_tab_index(
+    tabgroups: Query<
+        (&mut PaneTabgroup, &Children),
+        Or<(Changed<PaneTabgroup>, Changed<Children>)>,
+    >,
+) {
+    for (mut tabgroup, tabs) in tabgroups {
+        tabgroup.active_tab_index = tabgroup.active_tab_index.clamp(0, tabs.len() - 1);
+    }
+}
+
+pub struct PaneTabPlugin;
+
+impl Plugin for PaneTabPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Update, clamp_active_tab_index)
+            .add_systems(PostUpdate, focus_tabs);
+    }
+}
---
+++ src/pane/window.rs
@@ -0,0 +1,365 @@
+use bevy::{
+    app::{App, Plugin, Update},
+    asset::AssetServer,
+    camera::visibility::Visibility,
+    ecs::{
+        component::Component,
+        entity::Entity,
+        error::Result,
+        hierarchy::{ChildOf, Children},
+        lifecycle::Remove,
+        message::{Message, MessageReader},
+        observer::On,
+        schedule::IntoScheduleConfigs,
+        system::{Commands, EntityCommands, Query, Res, ResMut},
+    },
+    input_focus::InputFocus,
+    picking::{
+        Pickable,
+        events::{Pointer, Press},
+    },
+    ui::{FlexDirection, Node, Overflow, PositionType, UiRect, percent, px},
+    utils::default,
+    window::Window,
+};
+
+use crate::{
+    pane::{
+        PaneRegistrySystems, PaneTab, PaneTabbar, PaneTabgroup, Size, on_tab_drag,
+        on_tab_drag_cancel, on_tab_drag_end, on_tab_drag_start, on_tab_press, on_tabbar_drag_drop,
+        on_tabbar_drag_enter, on_tabbar_drag_leave, on_tabbar_drag_over, spawn_tab,
+        tab_context_menu,
+    },
+    theme::{
+        RoundedCorners, ThemeBackgroundColor, ThemeBorderColor,
+        tokens::{BUTTON_BG, PANE_BG, PANE_TAB_ACTIVE, WINDOW_BG},
+    },
+    widget::{EntityContextMenu, ScrollArea, ScrollAxis, Scrollbar, ScrollbarThumb},
+    window::{EditorWindowConfigured, EditorWindowStructure},
+};
+
+const PANE_BORDER_RADIUS: f32 = 6.0;
+
+#[derive(Message)]
+pub struct OpenPane {
+    pub name: String,
+}
+
+#[derive(Component)]
+pub(crate) struct PaneLayoutRoot;
+
+#[derive(Component)]
+pub(super) struct PaneRef {
+    pub entity: Entity,
+}
+
+pub(super) fn spawn_pane<'a>(
+    commands: &'a mut Commands,
+    focus: &mut InputFocus,
+    assets: &AssetServer,
+    size: f32,
+    tabs: Vec<String>,
+    focus_first: bool,
+) -> EntityCommands<'a> {
+    assert!(tabs.len() != 0, "Cannot spawn pane with no tabs.");
+
+    let root = commands.spawn((Node::default(), Size(size))).id();
+
+    let area = commands
+        .spawn((
+            ChildOf(root),
+            Node {
+                width: percent(100),
+                height: percent(100),
+                flex_direction: FlexDirection::Column,
+                border_radius: RoundedCorners::All.to_border_radius(PANE_BORDER_RADIUS),
+                ..default()
+            },
+            ThemeBackgroundColor(PANE_BG),
+        ))
+        .with_children(|commands| {
+            // Header
+            commands
+                .spawn((
+                    Node {
+                        width: percent(100),
+                        height: px(30),
+                        padding: UiRect::left(px(6)).with_right(px(8)),
+                        border: UiRect::horizontal(px(1)).with_top(px(1)),
+                        flex_shrink: 0.0,
+                        border_radius: RoundedCorners::Top.to_border_radius(PANE_BORDER_RADIUS),
+                        ..default()
+                    },
+                    ThemeBackgroundColor(WINDOW_BG),
+                    ThemeBorderColor::all(PANE_BG),
+                ))
+                .with_children(move |commands| {
+                    let scrollrect = commands
+                        .spawn((Node {
+                            width: percent(100),
+                            height: percent(100),
+                            ..default()
+                        },))
+                        .id();
+
+                    let tabgroup = commands
+                        .commands_mut()
+                        .spawn((
+                            ChildOf(scrollrect),
+                            Node {
+                                height: percent(100),
+                                width: percent(100),
+                                overflow: Overflow::scroll_x(),
+                                ..default()
+                            },
+                            Pickable {
+                                should_block_lower: false,
+                                ..default()
+                            },
+                            PaneTabgroup {
+                                active_tab_index: 0,
+                            },
+                            PaneRef { entity: root },
+                        ))
+                        .observe(
+                            move |_: On<Remove, Children>,
+                                  entities: Query<Entity>,
+                                  mut commands: Commands| {
+                                if entities.contains(root) {
+                                    commands.entity(root).despawn();
+                                }
+                            },
+                        )
+                        .id();
+
+                    commands
+                        .commands_mut()
+                        .entity(scrollrect)
+                        .insert(ScrollArea {
+                            target: tabgroup,
+                            vertical: false,
+                            main_axis: ScrollAxis::Horizontal,
+                            ..default()
+                        });
+
+                    let mut is_first_tab = true;
+                    for tab in tabs {
+                        let id = spawn_tab(commands.commands_mut(), assets, root, tab)
+                            .insert(ChildOf(tabgroup))
+                            .observe(on_tab_press)
+                            .observe(on_tab_drag_start)
+                            .observe(on_tab_drag)
+                            .observe(on_tab_drag_end)
+                            .observe(on_tab_drag_cancel)
+                            .id();
+
+                        if focus_first && is_first_tab {
+                            focus.set(id);
+                        }
+
+                        commands
+                            .commands_mut()
+                            .entity(id)
+                            .insert(EntityContextMenu(tab_context_menu(id)));
+
+                        is_first_tab = false;
+                    }
+
+                    let drop_indicator = commands
+                        .commands()
+                        .spawn((
+                            ChildOf(scrollrect),
+                            Node {
+                                position_type: PositionType::Absolute,
+                                height: percent(100),
+                                width: px(3),
+                                margin: UiRect::horizontal(px(-1)),
+                                left: px(0),
+                                ..default()
+                            },
+                            Pickable::IGNORE,
+                            ThemeBackgroundColor(PANE_TAB_ACTIVE),
+                            Visibility::Hidden,
+                        ))
+                        .id();
+
+                    commands
+                        .commands_mut()
+                        .spawn((
+                            ChildOf(scrollrect),
+                            Scrollbar {
+                                target: tabgroup,
+                                axis: ScrollAxis::Horizontal,
+                                ..default()
+                            },
+                            Node {
+                                position_type: PositionType::Absolute,
+                                bottom: px(0),
+                                width: percent(100),
+                                height: px(6),
+                                ..default()
+                            },
+                        ))
+                        .with_children(|commands| {
+                            commands.spawn((
+                                ScrollbarThumb,
+                                Node {
+                                    width: px(8),
+                                    height: percent(100),
+                                    position_type: PositionType::Absolute,
+                                    ..default()
+                                },
+                                ThemeBackgroundColor(BUTTON_BG),
+                            ));
+                        });
+
+                    // Tab drop area
+                    commands
+                        .commands_mut()
+                        .spawn((
+                            ChildOf(scrollrect),
+                            Node {
+                                position_type: PositionType::Absolute,
+                                width: percent(100),
+                                height: percent(100),
+                                ..default()
+                            },
+                            Pickable {
+                                should_block_lower: false,
+                                ..default()
+                            },
+                            PaneTabbar {
+                                drop_indicator,
+                                tabgroup,
+                            },
+                        ))
+                        .observe(on_tabbar_drag_enter)
+                        .observe(on_tabbar_drag_over)
+                        .observe(on_tabbar_drag_drop)
+                        .observe(on_tabbar_drag_leave);
+                });
+        })
+        .id();
+
+    let content = commands
+        .spawn((
+            ChildOf(area),
+            Node {
+                width: percent(100),
+                height: percent(100),
+                padding: UiRect::all(px(6)),
+                overflow: Overflow::hidden(),
+                ..default()
+            },
+        ))
+        .observe(
+            |trigger: On<Pointer<Press>>, mut focus: ResMut<InputFocus>| {
+                focus.set(trigger.entity);
+            },
+        )
+        .id();
+
+    commands.entity(root).insert(Pane { root, content });
+
+    commands.entity(root)
+}
+
+#[derive(Component, Clone, Copy)]
+pub struct Pane {
+    pub root: Entity,
+    pub content: Entity,
+}
+
+#[derive(Component)]
+struct SpawnedPaneWindow {
+    name: String,
+}
+
+fn open_panes(
+    mut requests: MessageReader<OpenPane>,
+    pane_tabs: Query<&PaneTab>,
+    mut commands: Commands,
+) {
+    for request in requests.read() {
+        if pane_tabs.iter().any(|tab| tab.name == request.name) {
+            // TODO: Focus tab
+            continue;
+        }
+
+        commands.spawn((
+            SpawnedPaneWindow {
+                name: request.name.clone(),
+            },
+            Window {
+                title: request.name.clone(),
+                transparent: true,
+                decorations: false,
+                ..default()
+            },
+        ));
+    }
+}
+
+fn setup_pane_window(
+    trigger: On<EditorWindowConfigured>,
+    spawned_pane_windows: Query<&SpawnedPaneWindow>,
+    editor_windows: Query<&EditorWindowStructure>,
+    assets: Res<AssetServer>,
+    mut focus: ResMut<InputFocus>,
+    mut commands: Commands,
+) -> Result {
+    let target = trigger.entity;
+
+    let Ok(spawned_pane_window) = spawned_pane_windows.get(target) else {
+        return Ok(());
+    };
+
+    let editor_window = editor_windows.get(trigger.entity)?;
+
+    commands
+        .entity(editor_window.content())
+        .with_children(|commands| {
+            commands
+                .spawn(Node {
+                    width: percent(100),
+                    height: percent(100),
+                    padding: UiRect::horizontal(px(4)).with_bottom(px(4)),
+                    ..default()
+                })
+                .with_children(|commands| {
+                    let root = commands.target_entity();
+                    spawn_pane(
+                        commands.commands_mut(),
+                        &mut focus,
+                        &assets,
+                        1.0,
+                        vec![spawned_pane_window.name.clone()],
+                        true,
+                    )
+                    .insert(ChildOf(root));
+                })
+                .observe(
+                    move |_: On<Remove, Children>,
+                          entities: Query<Entity>,
+                          mut commands: Commands| {
+                        if entities.contains(target) {
+                            commands.entity(target).despawn();
+                        }
+                    },
+                );
+        });
+
+    commands.entity(target).remove::<SpawnedPaneWindow>();
+
+    Ok(())
+}
+
+pub struct PaneWindowPlugin;
+
+impl Plugin for PaneWindowPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_message::<OpenPane>()
+            .add_systems(Update, open_panes.after(PaneRegistrySystems::Registration))
+            .add_observer(setup_pane_window);
+    }
+}
