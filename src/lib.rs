37fcd4602711e73642a51848f08a9821d487d596
gehud <grozdovnikolay@gmail.com>
2026-04-09 20:58:16 +0300

    chore: setup

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,12 @@
 use std::env;
 
-use bevy::{DefaultPlugins, app::{App, Plugin}};
+use bevy::{
+    DefaultPlugins,
+    app::{App, Plugin, Startup},
+    camera::Camera2d,
+    ecs::{error::Result, system::Commands},
+};
+use bevy_egui::{EguiContexts, EguiPlugin, EguiPrimaryContextPass, egui};
 
 pub const PLAY_MODE_VAR: &'static str = "BEVY_EDITOR_PLAY";
 
@@ -21,6 +27,21 @@ pub struct EditorPlugin;
 
 impl Plugin for EditorPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins(DefaultPlugins);
+        app.add_plugins(DefaultPlugins)
+            .add_plugins(EguiPlugin::default())
+            .add_systems(Startup, setup)
+            .add_systems(EguiPrimaryContextPass, ui);
     }
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2d);
+}
+
+fn ui(mut contexts: EguiContexts) -> Result {
+    egui::Window::new("Hello").show(contexts.ctx_mut()?, |ui| {
+        ui.label("world");
+    });
+
+    Ok(())
 }
