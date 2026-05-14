eab3b20d2dc8b53aef71c69ceecb176544b8204b
gehud <grozdovnikolay@gmail.com>
2026-05-14 18:11:38 +0300

    feat: configuring windows

--- src/window/mod.rs
+++ src/window/mod.rs
@@ -1,8 +1,75 @@
 use bevy::{
-    app::{App, Plugin},
+    app::{App, Last, Plugin},
+    camera::{Camera, Camera2d, RenderTarget},
+    color::Color,
+    ecs::{
+        component::Component,
+        entity::Entity,
+        query::{Added, With},
+        schedule::{IntoScheduleConfigs, SystemSet},
+        system::{Commands, Query},
+    },
+    ui::{BackgroundColor, Node, UiTargetCamera, percent},
     utils::default,
-    window::{Window, WindowPlugin},
+    window::{ExitCondition, PrimaryWindow, Window, WindowPlugin, WindowRef},
 };
+
+#[derive(Component)]
+pub struct EditorWindowStructure {
+    root: Entity,
+}
+
+impl EditorWindowStructure {
+    pub fn root(&self) -> Entity {
+        self.root
+    }
+}
+
+fn configure_windows(
+    windows: Query<Entity, Added<Window>>,
+    primary_windows: Query<Entity, With<PrimaryWindow>>,
+    mut commands: Commands,
+) {
+    let primary_window = primary_windows.single().ok();
+
+    for window in windows {
+        let window_ref = primary_window
+            .and_then(|primary_window| (primary_window == window).then_some(WindowRef::Primary))
+            .unwrap_or(WindowRef::Entity(window));
+
+        let camera = commands
+            .spawn((
+                Camera2d,
+                Camera {
+                    order: isize::MAX,
+                    ..default()
+                },
+                RenderTarget::Window(window_ref),
+            ))
+            .id();
+
+        let root = commands
+            .spawn((
+                UiTargetCamera(camera),
+                Node {
+                    width: percent(100),
+                    height: percent(100),
+                    ..default()
+                },
+                BackgroundColor(Color::BLACK),
+            ))
+            .id();
+
+        commands
+            .entity(window)
+            .insert(EditorWindowStructure { root });
+    }
+}
+
+#[derive(Debug, Hash, PartialEq, Eq, Clone, SystemSet)]
+pub enum EditorWindowSystems {
+    Configure,
+}
 
 pub struct EditorWindowPlugin;
 
@@ -13,7 +80,13 @@ impl Plugin for EditorWindowPlugin {
                 title: "Bevy Editor".into(),
                 ..default()
             }),
+            exit_condition: ExitCondition::OnPrimaryClosed,
             ..default()
-        });
+        })
+        .configure_sets(Last, EditorWindowSystems::Configure)
+        .add_systems(
+            Last,
+            configure_windows.in_set(EditorWindowSystems::Configure),
+        );
     }
 }
